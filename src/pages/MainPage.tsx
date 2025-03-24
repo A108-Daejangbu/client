f1e36c79aae595b1d8d561cb02a8a6c08d2b98cc
나혜원 <n417759@gmail.com>
2025-03-24 09:31:12 +0900

    #S12P21A108 :sparkles:[FEAT] 거래내역 컴포넌트 생성

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import TransactionList from "../features/Main/Transaction/TransactionList";
+import SearchBar from "../features/Main/SearchBar";
+
+import { accountInfo } from "../dummy/accountInfo";
 
 function MainPage() {
-  return <div className="content">MainPage</div>;
+  return(
+    <div className="content !pt-0">
+      <div className="w-full py-2">
+        <SearchBar />
+      </div>
+
+      <div className="text-[28px] font-pre-extrabold justify-self-center items-center py-4"><span>{accountInfo[0].accountName}</span></div>
+
+      <div className="flex h-screen">
+        <div className="w-1/6 justify-items-center">
+          <div>점수...</div>
+        </div>
+        <div className="w-2/3">
+          <TransactionList />
+        </div>
+        <div className="w-1/6 justify-items-center">
+          <div>영수증..</div>
+        </div>
+
+      </div>
+      <div></div>
+    </div>
+  );
 }
 
 export default MainPage;
