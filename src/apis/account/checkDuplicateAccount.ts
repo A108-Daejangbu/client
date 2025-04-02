ce75dc1e0449c270a3b999acfe1ff805affa771e
이가희 <lgh7956@naver.com>
2025-04-02 10:03:26 +0900

    🐛 [FIX] 계좌중복확인API 에러 해결 및 숫자유효성 추가

--- src/apis/account/checkDuplicateAccount.ts
+++ src/apis/account/checkDuplicateAccount.ts
@@ -1,7 +1,4 @@
-import axios, { AxiosResponse } from "axios";
 import axiosClient from "../axiosClient";
-// 전역에서 정의한 axios 인스턴스(axiosClient)를 불러와서 사용
-// 기존에 axios.post를 호출하던 부분을 axiosClient.post로 변경하여, 모든 요청에 대해 공통 설정(baseURL, withCredentials 등)을 적용
 
 interface CheckDuplicateAccountResponse {
   result: boolean;
@@ -12,16 +9,14 @@ export const checkDuplicateAccount = async (
   accountNo: string
 ): Promise<CheckDuplicateAccountResponse> => {
   try {
-    // 전역 axios 인스턴스를 사용하여 API 호출
-    // baseURL이 이미 axiosClient에 설정되어 있으므로,
-    // 여기서는 엔드포인트 경로만 지정.
-    const response: AxiosResponse<CheckDuplicateAccountResponse> =
-      await axiosClient.post("/account/manager/check-duplicate", { accountNo });
+    const response = await axiosClient.post<CheckDuplicateAccountResponse>(
+      "/account/manager/check-duplicate",
+      { accountNo }
+    );
     return response.data;
-  } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      return error.response.data as CheckDuplicateAccountResponse;
-    }
-    throw error;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error: unknown) {
+    // 모든 에러 케이스를 단일 메시지로 처리
+    throw new Error("계좌번호 검증 중 오류가 발생했습니다.");
   }
 };
